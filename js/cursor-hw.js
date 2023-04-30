52a7810e854e7a4ab8e6673bf48a1529f7952850
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2023-04-30 14:45:12 +0300

    task 8 done

--- js/cursor-hw.js
+++ js/cursor-hw.js
@@ -781,3 +781,151 @@ const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
 // let timeId = setInterval(() => getMySalary.call(latvia), 2000);
 // //clearInterval(timeId)
+
+////////////////////////////////task7//////////////////////////////////
+
+// У цьому завданні вам необхідно створити клас Student та розробляти методи
+// всередині цього класу.
+// 1.
+// У стдентів повинні бути наступні властивості: university, course, fullName,
+// вони передаються при створенні студента(в конструктор).
+// 2. Створіть метод this.getInfo() -> "Студент 1го курсу Вищої Школи Психотерапії
+// м.Одеса, Остап Родоманський Бендер", метод повертає сукупну інформацію про
+// курс, учбовий заклад та імені студента.
+// 3. Створіть геттер оцінок this.marks, який повертає масив оцінок студента [5, 4,
+// 4, 5]
+// 4. Створіть сеттер оцінок this.marks = 5, який дозволяє поставити оцінку
+// студенту. Після того, як оцінка поставлена, геттер повинен повернути масив
+// this.marks -> [5, 4, 4, 5, 5]
+// 5. Створіть метод отримання середнього балу this.getAverageMark() -> 4.6
+// 6. Створіть метод this.dismiss, який "виключить" студента. Після виклику цього
+// методу – ставити студенту оцінки та отримувати їх більше не можна. (Ніяких
+// помилок, просто повертається завжди null замість масиву оцінок)
+// 7. Створіть метод this.recover, який дозволить поновити студента
+// Advanced
+// 1.
+// Створіть новий клас BudgetStudent, який повністю наслідує клас Student
+// 2. Бюджетний студент може отримати стипендію за допомогою методу
+// this.getScholarship.
+// Отримання стипендії супроводжується виведенням інформації в консоль: Ви
+// отримали 1400 грн. стипендії
+// 3. Метод отримання стипендії автоматично викликається кожні 30 секунд післе
+// створення об'єкту. Підказка: викликайте його в constructor
+// 4. Студент отримує стипендію тільки в тому випадку, якщо середній бал у нього
+// вище або дорівнює 4.0
+// 5. Якщо студента виключено, він не отримує стипендію (думаю це було і так
+// очевидно :) )
+
+class Student {
+  constructor({university, course, fullName, marks = []}) {
+    this.university = university;
+    this.course = course;
+    this.fullName = fullName;
+    this.studMarks = marks;
+    this.excluded = true;
+  }
+
+  getInfo() {
+    //return this;
+    return (
+      "Студент " +
+      this.course +
+      "го курсу " +
+      this.university +
+      ", " +
+      this.fullName
+    );
+
+  }
+  
+  getAverageMark() {
+    const res = this.studMarks.reduce((prevValue, number) => {
+      const sum = prevValue + number;
+      return sum;
+    }, 0);
+    return res / this.studMarks.length;
+  }
+
+  dismiss() {
+    if (this.excluded === true) {
+      let MarksOfNull = this.studMarks;
+      MarksOfNull = null;
+      return (`Студента виключено: ${MarksOfNull}`);
+    }
+  }
+
+  recovery() {
+    this.excluded = false;
+    return (`Студента поновлено: ${this.studMarks}`);
+  }
+
+  get marks() {
+    if (this.excluded === false) {
+      return this.studMarks;
+    }
+    return null;
+  }
+
+  set marks(newMark) {
+    if (this.excluded === false) {
+      return this.studMarks.push(newMark);
+    }
+  }
+}
+
+class BudgetStudent extends Student {
+  constructor({university, fullName, marks, excluded, scholarship = 0}) {
+    super({university, fullName, marks, excluded});
+    this.scholarship = scholarship;
+    this.scholarshipInterval = setInterval(() => this.getScholarship(), 3000);
+  }
+
+  getScholarship() {
+  if (this.getAverageMark(this.marks) >= 4) {
+      console.log("Ви отримали 1400 грн. стипендії");
+      return this.scholarship += 1400;
+    } else 
+    if ((this.getAverageMark(this.marks) < 4) || (this.dismiss(this.excluded))) {
+      console.log("студента відчислено");
+      return this.scholarship;
+    }  
+      
+  }
+  stopInterval() {
+    setTimeout(() => clearInterval(this.scholarshipInterval), 30000);
+  }
+
+}
+const budgetStud1 = new BudgetStudent({
+  university: "Вища Школа Психотерапії м.Одеса",
+  course: 1,
+  fullName: "Остап Родоманський Бендер",
+  marks: [5, 4, 4, 5],
+  
+});
+console.log(budgetStud1);
+console.log(budgetStud1.getScholarship());
+budgetStud1.stopInterval();
+const student2 = new Student({
+  university: "Вищої Школи Психотерапії м.Одеса",
+  course: 1,
+  fullName: "Остап Родоманський Бендер",
+  marks: [5, 4, 4, 5],
+  
+});
+//console.log(student2);
+//console.log(student2.getInfo());
+//console.log(student2.marks);
+//student2.marks = 5;
+//console.log(student2.marks);
+//console.log(student2.getAverageMark());
+//console.log(student2.dismiss());
+//console.log(student2.recovery());
+///student2.marks = 5;
+//console.log(student2.marks);
+
+
+
+
+
+
