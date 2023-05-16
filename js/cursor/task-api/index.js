87609f480b69f653c1d5ce89780111992063f6f5
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2023-05-17 00:01:53 +0300

    add loadMore function

--- js/cursor/task-api/index.js
+++ js/cursor/task-api/index.js
@@ -64,21 +64,24 @@ async function getCharacters() {
     return getCharacters;
 
 };
-
+let page = 1;
 async function getPlanets() {
-    const planets = await fetch(`${BASE_URL}/planets/`)
+    const planets = await fetch(`${BASE_URL}/planets/?page=${page}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText);
             }
             return response.json();
         })
-        .then(data => console.log(data.results))
+        //.then(data => console.log(data.results))
         .catch(error => console.log(error.message));
     
     return planets;
 };
 
+//  function standby() {
+//         document.querySelector('.image').src = './images/star-wars.jpg';
+// }
 function getCharactersByFilmId(id) {
 
     return getFilmById(id).then(data => { return data.characters }).then(arr => arr.map(item => {
@@ -93,7 +96,7 @@ function getCharactersByFilmId(id) {
             .then(data => {
                 refs.gallery.innerHTML += (
                     `<li class="list-item">
-                       <img src="images/${data.name}.jpg" alt="hero pic" width="180" />
+                       <img src="images/${data.name}.jpg" alt="./images/star-wars.jpg" onerror="this.onerror=null;this.src=this.alt;" width="180" />
                        <h2 class="item-title">Name: ${data.name}</h2>
                        <p class="age-desc">Birth: ${data.birth_year}</p>
                        <p class="sex-desc">Sex: ${data.gender}</p>
@@ -103,7 +106,8 @@ function getCharactersByFilmId(id) {
             .catch(error => console.log(error.message));
     }))
 }
-    
+   
+
 const onInputHandle = (e) => {
     console.log(e.currentTarget.value);
 };
@@ -116,10 +120,25 @@ const onSearchFilm = (e) => {
 
 const onPlanetBtnClick = () => {
     console.log(getPlanets());
-}
+    refs.gallery.innerHTML = "";
+    getPlanets().then(({ results }) => {
+        console.log(results);
+        appendPlanetCard(results)
+    });
+};
+
+const onLoadMore = () => {
+    refs.gallery.innerHTML = "";
+    page += 1;
+    getPlanets().then(data => { return data }
+    ).then(({ results }) => { 
+    console.log(results);
+    appendPlanetCard(results)
+})
+};
 
 const markupFilmRender = (data) => {
-    return markupFilm = data.map((film) => {
+    return data.map((film) => {
         console.log(film);
         return `
         <li class="list-item">
@@ -138,15 +157,29 @@ const appendGalleryItemsMarkup = (data) => {
    refs.gallery.insertAdjacentHTML('beforeend', markupFilmRender(data));
 }
 
+const markupPlanetCardRender = (planets) => {
+    return planets.map((planet) => {
+        return `
+        <li class="list-item">
+          <img src="" alt="${planet.name} pic" />
+          <h2 class="item-title"> Name: ${planet.name}</h2>
+          <p class="age-desc">Rotation period: ${planet.rotation_period}</p>
+          <p class="sex-desc">Climat: ${planet.climate}</p>
+        </li>
+        `;
+    }).join(" ");
+
+    
+};
+
+const appendPlanetCard = (data) => {
+    refs.gallery.insertAdjacentHTML('beforeend', markupPlanetCardRender(data));
+}
+
+
 refs.input.addEventListener('input', onInputHandle);
 refs.filmBtn.addEventListener('click', onSearchFilm);
 refs.planetBtn.addEventListener('click', onPlanetBtnClick);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-// refs.characterBtn.addEventListener('click', () => {
-//   // let chapters = document.querySelector("input").value
 
-    
-//        // refs.gallery.innerHTML = ""
-//        getCharactersByFilmId(document.querySelector("input").value)
- 
-// })
