a53087628f78900ba34d26fde88550eb6e0e01f1
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2023-05-16 17:25:01 +0300

    add fetch planets

--- js/cursor/task-api/index.js
+++ js/cursor/task-api/index.js
@@ -65,6 +65,20 @@ async function getCharacters() {
 
 };
 
+async function getPlanets() {
+    const planets = await fetch(`${BASE_URL}/planets/`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => console.log(data.results))
+        .catch(error => console.log(error.message));
+    
+    return planets;
+};
+
 function getCharactersByFilmId(id) {
 
     return getFilmById(id).then(data => { return data.characters }).then(arr => arr.map(item => {
@@ -75,55 +89,58 @@ function getCharactersByFilmId(id) {
                         throw new Error(response.statusText);
                     }
                     return response.json()
-                   // .then(data => console.log(data))
-            }
-        )
+                })
+            .then(data => {
+                refs.gallery.innerHTML += (
+                    `<li class="list-item">
+                       <img src="images/${data.name}.jpg" alt="hero pic" width="180" />
+                       <h2 class="item-title">Name: ${data.name}</h2>
+                       <p class="age-desc">Birth: ${data.birth_year}</p>
+                       <p class="sex-desc">Sex: ${data.gender}</p>
+                    </li>`
+                )
+            })
+            .catch(error => console.log(error.message));
     }))
+}
     
-      
-    // }).then(a=>{userList.innerHTML+=(`<li class="gallery-item"><p class="gallery-name"><b>Name</b>: ${a.name}</p>
-    //   <p class="gallery-name"><b>birth year</b>: ${a.birth_year}</p>
-    //    <p class="gallery-name">sex</b>: ${a.gender}</p>
-    //        <img src="images/characters/${a.name}.jpg" alt="no pic" class="gallery-image-film" />
-    //     </li>`
-    // )
-    
-      
-    // })
-        
-    // }))
-   }
-//getFilms();
-//getCharactersByFilmId(3);
-
-
 const onInputHandle = (e) => {
     console.log(e.currentTarget.value);
 };
 
 const onSearchFilm = (e) => {
-    getCharactersByFilmId(refs.input.value)
-         .then(data => (markupFilmRender(data)))
-        .catch(error => console.log(error.message));
+    console.log(getCharactersByFilmId(refs.input.value))
+    //.then(appendGalleryItemsMarkup)
+         
 };
 
-const markupFilmRender = (films) => {
-    const markupFilm = films.map(film => {
+const onPlanetBtnClick = () => {
+    console.log(getPlanets());
+}
+
+const markupFilmRender = (data) => {
+    return markupFilm = data.map((film) => {
+        console.log(film);
         return `
         <li class="list-item">
-          <img src="" alt="hero pic" />
-          <h2 class="item-title"></h2>
-          <p class="age-desc"></p>
-          <p class="sex-desc"></p>
+          <img src="images/Chewbacca.jpg" alt="hero pic" />
+          <h2 class="item-title"> Name: ${film.name}</h2>
+          <p class="age-desc">Birth: ${film.birth_year}</p>
+          <p class="sex-desc">Sex: ${film.gender}</p>
         </li>
         `;
     }).join(" ");
 
-    refs.gallery.insertAdjacentHTML('beforeend', markupFilm);
+    
 };
+
+const appendGalleryItemsMarkup = (data) => {
+   refs.gallery.insertAdjacentHTML('beforeend', markupFilmRender(data));
+}
 
 refs.input.addEventListener('input', onInputHandle);
 refs.filmBtn.addEventListener('click', onSearchFilm);
+refs.planetBtn.addEventListener('click', onPlanetBtnClick);
 
 // refs.characterBtn.addEventListener('click', () => {
 //   // let chapters = document.querySelector("input").value
